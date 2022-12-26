c13c2bebb5cb0c580c36e4d819e2b95d0e53bb05
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-26 21:06:53 +0200

    Add INSERT examples

---
+++ insert.sql
@@ -0,0 +1,39 @@
+INSERT INTO cars
+VALUES (
+    'Best model',
+    2022,
+    19999.99,
+    false,
+    '26-12-2022'
+  );
+--
+INSERT INTO cars
+VALUES (
+    'Best model',
+    2022,
+    19999.99,
+    false,
+    '26-12-2022'
+  ),
+  (
+    'Best model',
+    2022,
+    19999.99,
+    false,
+    '26-12-2022'
+  ),
+  (
+    'Best model',
+    2022,
+    19999.99,
+    false,
+    '26-12-2022'
+  ),
+  (
+    'Best model',
+    2022,
+    19999.99,
+    false,
+    '26-12-2022'
+  );
+-- SELECT * FROM cars;
