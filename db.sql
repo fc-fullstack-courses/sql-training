93af72fa6d13d9e97be09584bb58fe88029205e5
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-26 19:13:36 +0200

    Add basic syntax (comments, comand, strings)

---
+++ db.sql
@@ -0,0 +1,19 @@
+-- Single line comment
+/*
+CREATE DATABASE TRAINING;
+CREATE DATABASE "TraINIng Taegaffdaf";
+CREATE DATABASE training;
+
+DROP DATABASE "camelCase";
+
+CREATE DATABASE test_db_123;
+*/
+
+
+'user'
+'user''s grant'
+
+'user''s'
+'grant'
+
+$$user's grant$$
