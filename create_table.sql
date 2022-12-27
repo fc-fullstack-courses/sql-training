ae3f6b1fc2acfc09058cdaa46b59dae82c6ced81
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-27 21:03:48 +0200

    Add task

--- create_table.sql
+++ create_table.sql
@@ -3,14 +3,16 @@
 CREATE TABLE cars (
   id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
   model varchar(100) NOT NULL CHECK (model != ''),
-  year_of_production int NOT NULL CHECK (year_of_production > 1900 AND year_of_production < 2024),
+  year_of_production int NOT NULL CHECK (
+    year_of_production > 1900
+    AND year_of_production < 2024
+  ),
   vin_number int NOT NULL UNIQUE,
   price numeric(8, 2) NOT NULL CHECK (price > 0),
   is_used boolean NOT NULL,
   created_at date NOT NULL DEFAULT current_date,
   updated_at date NOT NULL DEFAULT current_date,
-  UNIQUE (model, year_of_production) 
-  -- PRIMARY KEY (model, year_of_production)
+  UNIQUE (model, year_of_production) -- PRIMARY KEY (model, year_of_production)
 );
 DROP TABLE cars;
 /*
