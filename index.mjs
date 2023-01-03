535763e15afeb8b6fd08b13e53326bf0f0cf804e
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-03 18:33:10 +0200

    Add reset script

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
 import pg from 'pg';
+import fs from 'fs/promises';
 import { getUsers } from './api/index.mjs';
 import { mapUsers } from './utils/index.mjs';
 const { Client } = pg;
@@ -17,17 +18,21 @@ const users = await getUsers();
 
 await client.connect();
 
-const { rows } = await client.query(`
-  INSERT INTO users (
-    "name", 
-    "email", 
-    "password", 
-    "phone_num"
-    )
-  VALUES ${mapUsers(users)}
-  RETURNING *;
-`);
+const resetDBQuery = await fs.readFile('./sql/not-rozetka/tables.sql', 'utf-8');
 
-console.log(rows);
+await client.query(resetDBQuery);
+
+// const { rows } = await client.query(`
+//   INSERT INTO users (
+//     "name", 
+//     "email", 
+//     "password", 
+//     "phone_num"
+//     )
+//   VALUES ${mapUsers(users)}
+//   RETURNING *;
+// `);
+
+// console.log(rows);
 
 await client.end();
