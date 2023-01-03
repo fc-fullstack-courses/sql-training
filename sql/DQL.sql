0a9b88952452a7dac72c32a51f9ad5634323e413
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-03 20:37:32 +0200

    Add SELECT alias example

--- sql/DQL.sql
+++ sql/DQL.sql
@@ -2,6 +2,14 @@
 SELECT * -- говорим какие столбцы мы хоти получить
 FROM users; -- говорим из какой таблицы мы их хотим достать
 --
+SELECT first_name, birthday FROM users;
+--
+SELECT 2 + 2;
+--
+SELECT first_name AS "First name", birthday, 2 + 2 "sum of 2 + 2" FROM users;
+--
+SELECT extract (year from age(birthday)), *  FROM users;
+--
 SELECT *
 FROM users
 WHERE height < 2;
