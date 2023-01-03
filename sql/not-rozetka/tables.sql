11f028e5041c7063de9b7c1353bd78cff0025211
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-03 18:24:49 +0200

    Change db creation script

--- sql/not-rozetka/tables.sql
+++ sql/not-rozetka/tables.sql
@@ -1,9 +1,22 @@
+DROP TABLE IF EXISTS orders_to_products;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS reviews CASCADE;
+DROP TABLE IF EXISTS ratings CASCADE;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS users;
+
+-- CREATE TABLE IF NOT EXISTS users
 CREATE TABLE users (
   id serial PRIMARY KEY,
-  name varchar(256) NOT NULL CHECK (name != ''),
+  first_name varchar(256) NOT NULL CHECK (first_name != ''),
+  last_name varchar(256) NOT NULL CHECK (last_name != ''),
   email varchar(256) NOT NULL CHECK (email != ''),
   password varchar(256) NOT NULL CHECK (password != ''),
   phone_num varchar(20) NOT NULL CHECK(phone_num != ''),
+  birthday date CHECK (birthday < current_date),
+  is_male boolean,
+  height numeric(3,2) CHECK (height > 0.2),
+  weigth smallint CHECK (weigth < 1500),
   created_at timestamp NOT NULL DEFAULT current_timestamp,
   updated_at timestamp NOT NULL DEFAULT current_timestamp
 );
@@ -14,6 +27,7 @@ CREATE TABLE products (
   description varchar(5000),
   price numeric(10, 2) NOT NULL CHECK(price > 0),
   quantity int NOT NULL CHECK (quantity >= 0),
+  category NOT NULL CHECK (category != ''),
   -- могла быть еще таблица производителей
   manufacturer varchar (256) NOT NULL CHECK(manufacturer != ''),
   min_age smallint CHECK (min_age > 0),
@@ -34,7 +48,7 @@ CREATE TABLE reviews (
 CREATE TABLE ratings (
   id serial PRIMARY KEY,
   review_id int NOT NULL UNIQUE REFERENCES reviews,
-  rating numeric(2,1) NOT NULL CHECK(rating > 0 AND rating <= 10),
+  rating numeric(3,1) NOT NULL CHECK(rating > 0 AND rating <= 10),
   created_at timestamp NOT NULL DEFAULT current_timestamp,
   updated_at timestamp NOT NULL DEFAULT current_timestamp  
 );
@@ -52,7 +66,7 @@ CREATE TABLE orders (
 CREATE TABLE orders_to_products(
   order_id int REFERENCES orders,
   product_id int REFERENCES products,
-  quantity int NOT NULL CHECK (quantity > 1),
+  quantity int NOT NULL CHECK (quantity > 0),
   created_at timestamp NOT NULL DEFAULT current_timestamp,
   updated_at timestamp NOT NULL DEFAULT current_timestamp,
   PRIMARY KEY (order_id, product_id)
@@ -68,7 +82,7 @@ CREATE TABLE orders_to_products(
   (2, 2) +
   (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 5) -
 */
-ALTER TABLE users
-ADD COLUMN birthday date,
-ADD COLUMN height numeric(3,2) CHECK (height > 0.2),
-ADD COLUMN weigth smallint CHECK (weigth < 1500);
+-- ALTER TABLE users
+-- ADD COLUMN birthday date,
+-- ADD COLUMN height numeric(3,2) CHECK (height > 0.2),
+-- ADD COLUMN weigth smallint CHECK (weigth < 1500);
