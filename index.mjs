d9cf242ffa6fbba67f4dcdee32858cb6a234dfb2
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-03 20:18:15 +0200

    Add orders generation

--- index.mjs
+++ index.mjs
@@ -1,10 +1,16 @@
 import pg from 'pg';
 import fs from 'fs/promises';
+import _ from 'lodash';
 import config from './configs/db.json' assert { type: 'json' };
 import User from './models/User.mjs';
 import Product from './models/Product.mjs';
 import { getUsers } from './api/index.mjs';
+import generationConfig from './configs/generation.json' assert { type: 'json' };
+
 const { Client } = pg;
+const {
+  orders: { maxOrders, minOrders, maxQuantity, minQuantity },
+} = generationConfig;
 
 const client = new Client(config);
 
@@ -21,6 +27,51 @@ await client.query(resetDBQuery);
 const createdUsers = await User.bulkCreate(users);
 const products = await Product.bulkCreate();
 
-console.log(createdUsers);
+// создаем заказы
+const ordersString = createdUsers
+  .map((user) =>
+    new Array(_.random(minOrders, maxOrders, false))
+      .fill(undefined)
+      .map(() => `(${user.id})`)
+      .join(',')
+  )
+  .join(',');
+
+const { rows: orders } = await client.query(`
+INSERT INTO orders (
+  user_id
+)
+VALUES ${ordersString}
+RETURNING id;
+`);
+
+// наполнить заказы
+const ordersToProductsString = orders
+  .map((order) => {
+    // отбор продуктов
+    const productsInOrder = new Array(_.random(1, 5))
+      .fill(undefined)
+      .map(() => products[_.random(0, products.length - 1)]);
+
+    // отсортировать повторяющиеся продукты
+    const filteredProducts = [...new Set(productsInOrder)];
+    // вернуть строку типа (1, 4 , 18)
+    return filteredProducts
+      .map(
+        (product) =>
+          `(${order.id}, ${product.id}, ${_.random(minQuantity, maxQuantity)})`
+      )
+      .join(',');
+  })
+  .join(',');
+
+await client.query(`
+INSERT INTO orders_to_products (
+  order_id,
+  product_id,
+  quantity
+)
+VALUES ${ordersToProductsString};
+`);
 
 await client.end();
