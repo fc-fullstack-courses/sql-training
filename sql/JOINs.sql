02f8a4080c687c6468e58cbbe37256ea7a82569b
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-06 20:02:48 +0200

    Add some JOIN examples

--- sql/JOINs.sql
+++ sql/JOINs.sql
@@ -11,3 +11,26 @@ WHERE user_id = users.id;
 SELECT email, orders.*
 FROM orders
 JOIN users ON user_id = users.id;
+--
+SELECT *
+FROM ratings
+JOIN reviews ON review_id = reviews.id;
+-- товары из третьего заказа
+SELECT p.name, category, manufacturer
+FROM orders AS o
+JOIN orders_to_products AS otp ON o.id = otp.order_id
+JOIN products p ON otp.product_id = p.id
+WHERE o.id = 3;
+-- все заказы с клавиатурами
+SELECT o.*
+FROM orders o
+JOIN orders_to_products otp ON o.id = otp.order_id
+JOIN products p ON p.id = otp.product_id
+WHERE p.category = 'keyboards';
+-- сколько производителей в конкретном заказе
+SELECT manufacturer, count (manufacturer)
+FROM products p
+JOIN orders_to_products otp ON otp.product_id = p.id
+JOIN orders o ON o.id = otp.order_id
+WHERE o.id = 3
+GROUP BY manufacturer;
