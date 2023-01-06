fcd86843fd0e5bc65e866ee8a1f0b15f4f520e83
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-06 20:31:29 +0200

    Add JOIN types

--- sql/JOINs.sql
+++ sql/JOINs.sql
@@ -18,8 +18,8 @@ JOIN reviews ON review_id = reviews.id;
 -- товары из третьего заказа
 SELECT p.name, category, manufacturer
 FROM orders AS o
-JOIN orders_to_products AS otp ON o.id = otp.order_id
-JOIN products p ON otp.product_id = p.id
+INNER JOIN orders_to_products AS otp ON o.id = otp.order_id
+INNER JOIN products p ON otp.product_id = p.id
 WHERE o.id = 3;
 -- все заказы с клавиатурами
 SELECT o.*
@@ -34,3 +34,34 @@ JOIN orders_to_products otp ON otp.product_id = p.id
 JOIN orders o ON o.id = otp.order_id
 WHERE o.id = 3
 GROUP BY manufacturer;
+-- LEFT JOIN - все строки из левой таблицы
+-- все данные из правой таблицы
+-- там где данных для левой записи нет то засовуются нулы
+SELECT u.id, count(o.id)
+FROM users u -- левая таблица 
+LEFT JOIN orders o -- правая таблица
+ON o.user_id = u.id
+GROUP BY u.id
+ORDER BY u.id DESC;
+-- RIGHT JOIN - все строки из правой таблицы
+-- все данные из левой таблицы
+-- там где данных для правой записи нет то засовуются нулы
+SELECT u.id, count(o.id)
+FROM orders o
+RIGHT JOIN users u ON o.user_id = u.id
+WHERE o.user_id
+GROUP BY u.id
+ORDER BY u.id DESC;
+-- исключающий JOIN
+SELECT u.id
+FROM orders o
+RIGHT JOIN users u ON o.user_id = u.id
+WHERE o.user_id IS NULL -- волшебная часть
+GROUP BY u.id
+ORDER BY u.id DESC;
+-- FULL JOIN - данные из обеих таблиц где возомжно,
+-- нулы если нет каких то строк
+SELECT *
+FROM orders o
+FULL JOIN users u ON o.user_id = u.id
+ORDER BY u.id DESC;
