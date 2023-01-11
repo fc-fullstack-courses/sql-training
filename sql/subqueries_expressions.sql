f8852d3e48dfefbf1c98e95cc09b3f726d8a27e3
Kyrylo <fresh.js.ftd.@gmail.com>
2023-01-11 20:14:08 +0200

    Add IN / NOT IN

--- sql/subqueries_expressions.sql
+++ sql/subqueries_expressions.sql
@@ -1,8 +1,30 @@
 -- EXISTS
-SELECT * FROM products WHERE id = 4644651;
+SELECT *
+FROM products
+WHERE id = 4644651;
 --
 SELECT (EXISTS (SELECT * FROM products WHERE id = 95));
 --
 SELECT *
 FROM users
-WHERE EXISTS (SELECT * FROM orders WHERE user_id = users.id );
+WHERE EXISTS (
+    SELECT *
+    FROM orders
+    WHERE user_id = users.id
+  );
+-- IN / NOT IN
+SELECT *
+FROM users
+WHERE id IN (
+    SELECT user_id
+    FROM orders
+  );
+--
+SELECT users.*
+FROM users
+  JOIN orders on user_id = users.id
+WHERE users.id NOT IN (
+    SELECT user_id
+    FROM reviews
+  )
+GROUP BY users.id;
